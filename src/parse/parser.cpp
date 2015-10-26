ac356cb2c90ba35dfd22ee82829241226db9f080
Brandon Surmanski <b.surmanski@gmail.com>
2015-10-25 21:22:17 -0700

    updated parser

--- src/parse/parser.cpp
+++ src/parse/parser.cpp
@@ -1,13 +1,18 @@
 #include "parser.hpp"
 
+#include "exception/exception.hpp"
+
 #include <assert.h>
 
-Parser::Parser(Program *_program, Lexer *_lex) : program(_program), lex(_lex), peek(lex->lex()) {
+Parser::Parser(Program *_program, Lexer *_lex) : program(_program), lex(_lex) {
 }
 
 Parser::~Parser() {
-    delete program;
-    delete lex;
+    while(!tbuf.empty()) {
+        tbuf.pop();
+    }
+    program->release();
+    lex->release();
 }
 
 Module *Parser::parse() {
@@ -16,52 +21,227 @@ Module *Parser::parse() {
     return module;
 }
 
-Token &Parser::peekTok() {
-    return peek;
+Lexer *Parser::getLexer() {
+    return lex;
 }
 
-void Parser::ignoreTok() {
-    peek = lex->lex();
+Token &Parser::peekTok(int i) {
+    while(tbuf.length() < i+1) {
+        tbuf.push(lex->lex());
+    }
+
+    return tbuf.peek(i);
+}
+
+bool Parser::ignoreTok() {
+    if(tbuf.empty()) {
+        lex->lex();
+    } else {
+        tbuf.pop();
+    }
+    return true;
 }
 
 Token Parser::getTok() {
-    Token t = peek;
-    peek = lex->lex();
-    return t;
-}
-
-Stmt *Parser::parseStmt() {
-    Token token = peekTok();
-
-    switch(token.getKind()) {
-        case tok::lbrace:
-            return parseCompoundStmt();
-        case tok::kw_goto:
-        case tok::kw_break:
-            ignoreTok();
-            return new BreakStmt;
-
-        case tok::kw_continue:
-            ignoreTok();
-            return new ContinueStmt;
-
-        case tok::kw_return:
-            return parseReturnStmt();
+    if(!tbuf.empty()) {
+        return tbuf.pop();
+    } else {
+        return lex->lex();
     }
 }
 
+Stmt *Parser::parseStmt() {
+    Token &token = peekTok();
+
+    Stmt *stmt = NULL;
+
+    switch(token.getKind()) {
+        case tok::lbrace:
+            stmt = parseCompoundStmt();
+            break;
+
+        case tok::kw_label:
+           stmt = parseLabelStmt();
+           break;
+
+        case tok::kw_goto:
+            stmt = parseGotoStmt();
+            break;
+
+        case tok::kw_break:
+            stmt = parseBreakStmt();
+            break;
+
+        case tok::kw_continue:
+            stmt = parseContinueStmt();
+            break;
+
+        case tok::kw_return:
+            stmt = parseReturnStmt();
+            break;
+
+        case tok::kw_case:
+            stmt = parseCaseStmt();
+            break;
+
+        case tok::kw_if:
+            stmt = parseIfStmt();
+            break;
+
+        //TODO: use_stmt, import_stmt
+
+        default:
+            stmt = parseExpr();
+
+            //TODO: augmented assign, eg: +=, -=, *=
+            if(peekTok().is(tok::equal)) {
+                ignoreTok(); // ignore '='
+                stmt = new AssignStmt((Expr*) stmt, parseExpr());
+            }
+            break;
+    }
+
+    do {
+        if(!peekTok().isTerminator() && !peekTok().is(tok::eof)) {
+            SourceLocation loc = peekTok().getSourceLocation();
+            throw new Exception(String("Expected line-terminator at line ") + String::fromInt(loc.getLine()));
+        }
+    } while(peekTok().isTerminator());
+
+    //TODO: stmt terminator
+    return stmt;
+}
+
 CompoundStmt *Parser::parseCompoundStmt() {
+    if(peekTok().getKind() != tok::lbrace || !ignoreTok()) {
+        throw new Exception("expected left brace beginning compound stmt");
+    }
+
+    DynArray<Stmt*> stmts;
+    while(peekTok().getKind() != tok::rbrace) {
+        Stmt *stmt = parseStmt();
+        stmts.append(stmt);
+    }
+
+    if(peekTok().getKind() != tok::lbrace || !ignoreTok()) {
+        throw new Exception("expected right brace following compound stmt");
+    }
+
+    return new CompoundStmt(stmts);
+}
+
+LabelStmt *Parser::parseLabelStmt() {
+    throw new Exception("unimplemented: parse label stmt");
+}
+
+GotoStmt *Parser::parseGotoStmt() {
+    throw new Exception("unimplemented: parse goto stmt");
+}
+
+BreakStmt *Parser::parseBreakStmt() {
+    throw new Exception("unimplemented: parse break stmt");
+}
+
+ContinueStmt *Parser::parseContinueStmt() {
+    throw new Exception("unimplemented: parse continue stmt");
 }
 
 ReturnStmt *Parser::parseReturnStmt() {
-    assert(peekTok().getKind() == tok::kw_return);
+    if(peekTok().getKind() != tok::kw_return) {
+        throw new Exception("expected 'return' keyword");
+    }
     ignoreTok();
     Expr *ex = parseExpr();
     //TODO
+    throw new Exception("unimplemented: parse return stmt");
+}
+
+CaseStmt *Parser::parseCaseStmt() {
+    throw new Exception("unimplemented: parse case stmt");
+}
+
+IfStmt *Parser::parseIfStmt() {
+    throw new Exception("unimplemented: parse if stmt");
 }
 
 Expr *Parser::parseExpr() {
+    throw new Exception("unimplemented: parse expr");
 }
 
 Decl *Parser::parseDecl() {
+    throw new Exception("unimplemented: parse decl");
+}
+
+Type *Parser::parseType() {
+
+}
+
+PrimativeType *Parser::parsePrimativeType() {
+    Token &token = peekTok();
+    PrimativeType *type = NULL;
+
+    switch(token.getKind()) {
+        case tok::kw_bool:
+            type = PrimativeType::getBool();
+            break;
+
+        case tok::kw_void:
+            type = PrimativeType::getVoid();
+            break;
+
+        case tok::kw_char:
+        case tok::kw_int8:
+            type = PrimativeType::getInt8();
+            break;
+
+        case tok::kw_short:
+        case tok::kw_int16:
+            type = PrimativeType::getInt16();
+            break;
+
+        case tok::kw_int:
+        case tok::kw_int32:
+            type = PrimativeType::getInt32();
+            break;
+
+        case tok::kw_long:
+        case tok::kw_int64:
+            type = PrimativeType::getInt64();
+            break;
+
+        case tok::kw_uchar:
+        case tok::kw_uint8:
+            type = PrimativeType::getUInt8();
+            break;
+
+        case tok::kw_ushort:
+        case tok::kw_uint16:
+            type = PrimativeType::getUInt16();
+            break;
+
+        case tok::kw_uint:
+        case tok::kw_uint32:
+            type = PrimativeType::getUInt32();
+            break;
+
+        case tok::kw_ulong:
+        case tok::kw_uint64:
+            type = PrimativeType::getUInt64();
+            break;
+
+        case tok::kw_float:
+        case tok::kw_float32:
+            type = PrimativeType::getFloat32();
+            break;
+
+        case tok::kw_double:
+        case tok::kw_float64:
+            type = PrimativeType::getFloat64();
+            break;
+
+        default:
+            throw new Exception("Unknown primative type");
+    }
+
+    return type;
 }
--- src/parse/parser.hpp
+++ src/parse/parser.hpp
@@ -1,35 +1,71 @@
 #ifndef _PARSER_HPP
 #define _PARSER_HPP
 
-#include "../program.hpp"
-#include "../lex/lexer.hpp"
-#include "../ast/module.hpp"
-#include "../ast/ast.hpp"
+#include "program.hpp"
+#include "lex/lexer.hpp"
+#include "ast/module.hpp"
+#include "ast/ast.hpp"
+#include "common/buffer.hpp"
 
-class Parser {
+class Parser : public Object {
     Program *program;
     Lexer *lex;
 
     Module *module;
 
-    Token peek;
+    Buffer<Token> tbuf;
 
     public:
     Parser(Program *_program, Lexer *_lex);
     ~Parser();
     Module *parse();
+    Lexer *getLexer();
 
     protected:
-    Token &peekTok();
-    void ignoreTok();
+    Token &peekTok(int i = 0);
+    bool ignoreTok();
     Token getTok();
 
+    public:
     Stmt *parseStmt();
     Expr *parseExpr();
     Decl *parseDecl();
+    Type *parseType();
 
-    CompoundStmt    *parseCompoundStmt();
-    ReturnStmt      *parseReturnStmt();
+    CompoundStmt        *parseCompoundStmt();
+    LabelStmt           *parseLabelStmt();
+    GotoStmt            *parseGotoStmt();
+    BreakStmt           *parseBreakStmt();
+    ContinueStmt        *parseContinueStmt();
+    ReturnStmt          *parseReturnStmt();
+    CaseStmt            *parseCaseStmt();
+    IfStmt              *parseIfStmt();
+
+    BinaryExpr          *parseBinaryExpr();
+    UnaryExpr           *parseUnaryExpr();
+    CharLiteralExpr     *parseCharLiteralExpr();
+    StringLiteralExpr   *parseStringLiteralExpr();
+    BoolLiteralExpr     *parseBoolLiteralExpr();
+    NullLiteralExpr     *parseNullLiteralExpr();
+    NumericLiteralExpr  *parseNumericLiteralExpr();
+    TupleExpr           *parseTupleExpr();
+    NewExpr             *parseNewExpr();
+    DeleteExpr          *parseDeleteExpr();
+    RetainExpr          *parseRetainExpr();
+    ReleaseExpr         *parseReleaseExpr();
+    IdExpr              *parseIdExpr();
+    PackExpr            *parsePackExpr();
+    MemberExpr          *parseMemberExpr();
+    CallExpr            *parseCallExpr();
+    IndexExpr           *parseIndexExpr();
+    CastExpr            *parseCastExpr();
+
+    PrimativeType       *parsePrimativeType();
+    UserType            *parseUserType();
+    TupleType           *parseTupleType();
+    FunctionType        *parseFunctionType();
+    PointerType         *parsePointerType();
+    ArrayType           *parseArrayType();
 };
 
 #endif
